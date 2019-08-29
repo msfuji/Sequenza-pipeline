3d6f312e08fcddd1463529700bda6c3a4be4f536
Masashi Fujita <mssfjt@gmail.com>
2019-08-29 16:57:59 +0900

    omit gc

--- scripts/sequenza-command.sh
+++ scripts/sequenza-command.sh
@@ -2,13 +2,45 @@
 
 set -eu
 
-sample_id=$1
-tumor_bam=$2
-normal_bam=$3
-reference_fasta=$4
-gc_wiggle=$5
-num_threads=$6
+#
+# parse args
+#
+usage() {
+  cat << EOS
+Usage: `basename $0` [options]
+  -s SAMPLE_ID    Sample ID (required)
+  -t TUMOR_BAM    Tumor BAM file (required)
+  -n NORMAL_BAM   Normal BAM file (required)
+  -r REF_FASTA    FASTA file of GRCh37 (required)
+  -c THREADS      The number of threads (optional; default 1)
+  -h              Display help message
+EOS
+  exit 1
+}
 
+num_threads=1
+while getopts s:t:n:r:c:h OPT; do
+  case $OPT in
+    s ) sample_id=$OPTARG;;
+    t ) tumor_bam=$OPTARG;;
+    n ) normal_bam=$OPTARG;;
+    r ) reference_fasta=$OPTARG;;
+    c ) num_threads=$OPTARG;;
+    h ) usage;;
+    ? ) usage;;
+  esac
+done
+
+#
+# compute GC contents
+#
+reference_base=`basename $reference_fasta | sed -e 's/\.fa$\|\.fas$\|\.fasta$//'`
+gc_wiggle="${reference_base}.gc50Base.wig.gz"
+/usr/local/bin/sequenza-utils gc_wiggle -w 50 --fasta $reference_fasta -o $gc_wiggle
+
+#
+# run Sequenza
+#
 chromosomes="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y"
 
 sequenza-utils bam2seqz -n ${normal_bam} -t ${tumor_bam} \
