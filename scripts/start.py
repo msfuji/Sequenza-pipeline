779d4114ad5e7f43cd22c2aff2f711b6b034d499
Masashi Fujita <mssfjt@gmail.com>
2019-08-30 10:59:01 +0900

    initial

--- scripts/start.py
+++ scripts/start.py
@@ -2,32 +2,73 @@
 
 import os
 import sys
-import sample_conf as sc
 import subprocess
+import contextlib
+import json
+import sample_conf as sc
 import genomon_conf as gc
 
 
-genomon_conf_file = sys.argv[1]
-sample_conf_file = sys.argv[2]
+@contextlib.contextmanager
+def pushd(new_dir):
+    old_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(old_dir)
 
-##
-gc.genomon_conf.read(genomon_conf_file)
-ref_fasta = gc.genomon_conf.get("REFERENCE", "ref_fasta")
-##
-sample_conf = sc.Sample_conf()
-sample_conf.parse_file(sample_conf_file)
 
-def analyze_tn_pair(tumor_id, tumor_bam, normal_id, normal_bam):
-    outdir="output/%s" % tumor_id
-    command = "qsub -l os6 -cwd -j y -o log analyze_tn_pair.sh %s %s %s %s %s" \
-              % (tumor_id, tumor_bam, normal_id, normal_bam, outdir)
-    print command
+def make_config(tumor_id, tumor_bam, normal_bam, ref_fasta, n_threads):
+    dict = {
+    'sample-id': tumor_id,
+    'tumor-bam': {'class': 'File', 'path': tumor_bam},
+    'normal-bam': {'class': 'File', 'path': normal_bam},
+    'reference-fasta': {'class': 'File', 'path': ref_fasta},
+    'num-threads': n_threads
+    }
+    config_file = 'config.json'
+    with open(config_file, 'w') as f:
+        json.dump(dict, f)
+    return config_file
+
+
+def analyze_tn_pair(run_script, config_file):
+    command = "qsub -l os7 -cwd -j y {} {}".format(run_script, config_file)
     subprocess.call(command, shell=True)
 
 
-os.mkdir("log")
-os.mkdir("output")
-for tumor_id, normal_id, _ in conf.mutation_call:
-    tumor_bam = conf.bam_import[tumor_id]
-    normal_bam = conf.bam_import[normal_id]
-    analyze_tn_pair(tumor_id, tumor_bam, normal_id, normal_bam)
+################################################################################
+#
+# parse args
+#
+if len(sys.argv) != 3:
+    print("Usage: python start.py [genomon.cfg] [sample.csv]\n")
+    exit(1)
+genomon_conf_file = sys.argv[1]
+sample_conf_file = sys.argv[2]
+n_threads = 8
+
+#
+# read config files
+#
+gc.genomon_conf.read(genomon_conf_file)
+ref_fasta = gc.genomon_conf.get("REFERENCE", "ref_fasta")
+sample_conf = sc.Sample_conf()
+sample_conf.parse_file(sample_conf_file)
+
+#
+# analyze pairs of tumor/normal
+#
+run_script = os.path.join(os.getcwd(), 'scripts', 'run-cwl.sh')
+for tumor_id, normal_id, _ in sample_conf.mutation_call:
+    tumor_bam = sample_conf.bam_import[tumor_id]
+    normal_bam = sample_conf.bam_import[normal_id]
+
+    # make and move into output directory
+    outdir = os.path.join('output', tumor_id)
+    os.makedirs(outdir)
+    with pushd(outdir):
+        config_file = make_config(tumor_id, tumor_bam, normal_bam,
+            ref_fasta, n_threads)
+        analyze_tn_pair(run_script, config_file)
